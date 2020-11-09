d7da5490fb3cf17c5a02780439979076b9aafe8b
syzhang <syzhang@outlook.com>
2020-11-09 17:17:25 +0000

    vba csv

---
+++ vba/run_compare_family.m
@@ -0,0 +1,31 @@
+function [ep, pep] = run_compare_family(output_dir)
+% run function compare_bms
+% output_dir - output dir of lap_*.mat
+
+    % list dir
+    wildcard_str = 'lap*.mat';
+    lap_list = dir([output_dir, filesep, wildcard_str]);
+
+    % lap names
+    lap_names = {};
+    lap_paths = {};
+    for f=1:length(lap_list)
+        lap_paths{f} = [lap_list(f).folder, filesep, lap_list(f).name];
+        lap_names{f} = lap_list(f).name(5:end-4);
+    end
+
+    % BMS
+    [posterior, out] = compare_bms(lap_paths);
+    % exceedance prob
+    ep = out.ep;
+    % protected ep
+    pep = (1-out.bor)*out.ep + out.bor/length(out.ep);
+
+    % save output
+    T = table(lap_names', ep', pep', 'VariableNames',{'model_name','exceedance_prob', 'protected_exceedance_prob'});
+    if contains(output_dir, 'practice')
+        writetable(T,'./output/practice.csv')
+    else
+        writetable(T,'./output/fmri.csv')
+    end
+end
